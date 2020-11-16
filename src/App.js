a0e991b6b038a5ae73bd2179b5bb46752b3b01ca
Anu Singh <anusingh1883656@gmail.com>
2020-11-16 16:45:39 +0530

    Food-recipe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import "./App.css"
+import Axios from "axios"
 
-function App() {
+const App = () => {
+
+  const [query, setQuery] = useState('')
+  const [recipes, setRecipes] = useState([]);
+
+
+  const APP_ID = "7936d2f6"
+  const APP_KEY = "f29c70bfd3ee74dc112aad0df5e8d6d1"
+  const url = `https://api.edamam.com/search?$q=${query}&$app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`
+
+  const getData = async () => {
+    const result = await Axios.get(url)
+    console.log(result)
+
+    setRecipes(result.data.hits);
+    setQuery('')
+  }
+
+  const onChange = (e) => {
+    setQuery(e.target.value)
+
+  }
+  const onSubmit = (e) => {
+    e.preventDefault()
+    getData()
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <h1 onClick={getData}>Food searching App</h1>
+      <form className="search-form" onSubmit={onSubmit}>
+        <input type="text" placeholder="search Food"
+          autoComplete="off"
+          onChange={onChange}
+          value={query}
+        />
 
-export default App;
+        <input type="submit" value="search"></input>
+
+      </form>
+
+    </div>
+  )
+}
+export default App
