5b42e65a94d1a0551609b6bf111bc77823c3f324
Anu Singh <anusingh1883656@gmail.com>
2020-11-17 10:19:47 +0530

    food-searching

--- src/App.js
+++ src/App.js
@@ -2,22 +2,32 @@ import React, { useState } from 'react'
 import "./App.css"
 import Axios from "axios"
 
+import Alert from "./components/Alert"
+import Recipe from "./components/Recipe"
+import { v4 as uuidv4 } from "uuid";
 const App = () => {
 
   const [query, setQuery] = useState('')
   const [recipes, setRecipes] = useState([]);
+  const [alert, setAlert] = useState('')
+  const APP_ID = "4e9f05eb";
+  const APP_KEY = "9b904d703fa0d46a88ce1ac63f29f498";
 
-
-  const APP_ID = "7936d2f6"
-  const APP_KEY = "f29c70bfd3ee74dc112aad0df5e8d6d1"
-  const url = `https://api.edamam.com/search?$q=${query}&$app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
   const getData = async () => {
-    const result = await Axios.get(url)
-    console.log(result)
-
-    setRecipes(result.data.hits);
-    setQuery('')
+    if (query !== '') {
+      const result = await Axios.get(url)
+      if (!result.data.more) {
+        return setAlert("No food with such name")
+      }
+      setRecipes(result.data.hits)
+      console.log(result)
+      setAlert("")
+      setQuery('')
+    } else {
+      setAlert("please fill the form")
+    }
   }
 
   const onChange = (e) => {
@@ -33,6 +43,7 @@ const App = () => {
     <div className="App">
       <h1 onClick={getData}>Food searching App</h1>
       <form className="search-form" onSubmit={onSubmit}>
+        {alert !== "" && <Alert alert={alert} />}
         <input type="text" placeholder="search Food"
           autoComplete="off"
           onChange={onChange}
@@ -42,6 +53,13 @@ const App = () => {
         <input type="submit" value="search"></input>
 
       </form>
+      <div className="recipes">
+        {recipes !== [] && recipes.map(recipe =>
+          <Recipe
+            key={uuidv4()}
+            recipe={recipe} />
+        )}
+      </div>
 
     </div>
   )
